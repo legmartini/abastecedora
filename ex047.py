d721ca5bb2b5cb8df992e8619cb7636df21cc1b5
Luiz Eduardo Griza Martini <luizmartini92@outlook.com>
2019-03-11 17:39:44 -0300

    Delete ex047.py

--- ex047.py
+++
@@ -1,6 +0,0 @@
-nota = float(input('Nota (0/10): '))
-notas = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-
-while nota not in notas:
-    nota = float(input('Nota inválida, tente novamente. Nota (0/10): '))
-print(f'Nota registrada com sucesso. NOTA: {nota}')
